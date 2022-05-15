6af8e8222399bd8204555e1d7124fbddd00c21a4
jianhua1 <94123409@qq.com>
2022-05-15 20:29:43 +0800

    init gshop

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from './router'
+
+//以阻⽌vue在启动时⽣成⽣产提⽰
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
